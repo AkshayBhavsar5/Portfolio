ff46b1b405a3b23964867290e75c1220acab378a
[200~AkshayBhavsar5git config --global user.name AkshayBhavsar5git config --global user.name [200~AkshayBhavsar5 <bhavsarakshay786@gmail.com>
2024-10-28 12:46:58 +0530

    databes locaion change and 1 new features of messages added

--- Backend/Controller/messageController.js
+++ Backend/Controller/messageController.js
@@ -3,15 +3,35 @@ import ErrorHandler from "../middlewares/error.js";
 import { Message } from "../models/messageSchema.js";
 
 export const sendMessage = catchAsyncError(async (req, res, next) => {
-
-  const {senderName , subject, message}= req.body;
-  if(!senderName || !subject || !message){
-    return next (new ErrorHandler("Please Fill Full Form",400))
-  } 
-  const data = await Message.create({senderName , subject, message}) 
+  const { senderName, subject, message } = req.body;
+  if (!senderName || !subject || !message) {
+    return next(new ErrorHandler("Please Fill Full Form", 400));
+  }
+  const data = await Message.create({ senderName, subject, message });
   res.status(200).json({
-    success:true,
-    message:"message sent",
-    data, 
-  })
+    success: true,
+    message: "message sent",
+    data,
+  });
+});
+
+export const getAllMessages = catchAsyncError(async (res, req, next) => {
+  const messages = await Message.find();
+  res.status(200).json({
+    success: true,
+    messages,
+  });
+});
+
+export const deleteMessage = catchAsyncError(async (res, req, next) => {
+  const { id } = req.params;
+  const message = await Message.findById(id);
+  if (!message) {
+    return next(new ErrorHandler("Message already Deleated!", 400));
+  }
+  await message.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Message Deleted Succesfuly... ",
+  });
 });
--- Backend/Router/messageRoutes.js
+++ Backend/Router/messageRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { sendMessage } from "../Controller/messageController.js";
+import { getAllMessages, sendMessage } from "../Controller/messageController.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage); 
+router.get("/getall", getAllMessages); 
+
 
 export default router;
